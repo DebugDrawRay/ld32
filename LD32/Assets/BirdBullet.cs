9a140eb02f4dd337a4097a8c6328cf9f1ce5780a
adam <imposter110@gmail.com>
2015-04-20 06:11:19 -0700

    Added da bird

---
+++ LD32/Assets/BirdBullet.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdBullet : MonoBehaviour {
+	public float speed;
+
+	public Vector3 direct;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Rigidbody> ().velocity = direct * speed * Time.deltaTime;
+	}
+}
